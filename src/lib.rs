de0e910ef091f654f50d0348b3ad6f18df958ae9
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-04 19:50:36 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,33 @@
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{input}' transform when every letter is shifted using key '{key}'?"
-    );
+    //  size of the alphabet
+    let alphabet_size = 26;
+
+    // Convert the key to a positive value within the range of the alphabet size
+    let key = (key % alphabet_size as i8 + alphabet_size as i8) % alphabet_size as i8;
+
+    // Creating Empty string 
+    let mut ciphertext = String::new();
+
+    // Moving over every character
+    for c in input.chars() {
+        // Is it capital
+        if c.is_ascii_alphabetic() && c.is_ascii_uppercase() {
+            // Changing character to ASCII value,
+            //putting it back
+            let rotated_ascii = (((c as u8 - b'A') as i8 + key) % alphabet_size as i8 + b'A' as i8) as u8;
+            // Convert the rotated ASCII value back to a character and append it to the ciphertext
+            ciphertext.push(rotated_ascii as char);
+        } else if c.is_ascii_alphabetic() && c.is_ascii_lowercase() {
+            // Then for lowercase characters
+            let rotated_ascii = (((c as u8 - b'a') as i8 + key) % alphabet_size as i8 + b'a' as i8) as u8;
+            ciphertext.push(rotated_ascii as char);
+        } else {
+            // save if not an alphabetic
+            ciphertext.push(c);
+        }
+    }
+
+    // Return the ciphertext
+    ciphertext
 }
+
